4a2a99c3880089cc0e7b8f0cba47ecde9a84fb8c
Alexa Stapleton <agstapleton@wisc.edu>
2023-04-17 14:34:08 -0500

    trying to get user to firestore

--- public/app.js
+++ public/app.js
@@ -18,17 +18,60 @@ signin_modalbg.addEventListener("click", () => {
   signin_modal.classList.remove("is-active");
 });
 
+// get the email and password
+
+let email = r_e("email_").value;
+let password = r_e("password_").value;
+
+// send email and password to firebase to create the user
+
+auth.createUserWithEmailAndPassword(email, password).then((user) => {
+  //configure_message_bar(`Welcome ${auth.currentUser.email}`);
+
+  // reset the form
+  r_e("signup_form").reset();
+
+  // hide the modal
+  r_e("signup_modal").classList.remove("is-active");
+});
+// sign out users
+
+r_e("signoutbtn").addEventListener("click", () => {
+  auth.signOut().then(() => {});
+});
+
+// sign in users
+
+r_e("signin_form").addEventListener("submit", (e) => {
+  // prevent the page from auth refresh
+  e.preventDefault();
+
+  // get the email and password from the form
+
+  let email = r_e("email_").value;
+  let password = r_e("password_").value;
+
+  // send email/password to firebase for authentication
+
+  auth.signInWithEmailAndPassword(email, password).then((user) => {
+    // reset the form
+    r_e("signin_form").reset();
+
+    // hide the modal
+    r_e("signin_modal").classList.remove("is-active");
+  });
+});
 
 // User Accounts
-let user1 = {
-  name: 'admin1',
-  admin: true
-};
+//let user1 = {
+// name: 'admin1',
+// admin: true
+//};
 
-let user2 = {
-  name: 'nonadmin1',
-  admin: false
-};
+//let user2 = {
+// name: 'nonadmin1',
+// admin: false
+//};
 
-db.collection("users").add(user1);
-db.collection("users").add(user2);
+//db.collection("users").add(user1);
+//db.collection("users").add(user2);
