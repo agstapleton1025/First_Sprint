3b7d3d4957cca62020d539164303a8358c682475
Alexa Stapleton <agstapleton@wisc.edu>
2023-04-16 19:34:00 -0500

    adding variables and sign in modal

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+//id function
+function r_e(id) {
+  return document.querySelector(`#${id}`);
+}
 
+// global variables
+
+let signinbtn = document.querySelector("#signinbtn");
+let signin_modal = document.querySelector("#signin_modal");
+let signin_modalbg = document.querySelector("#signin_modalbg");
+
+// sign-in modal link
+signinbtn.addEventListener("click", () => {
+  signin_modal.classList.add("is-active");
+});
+
+signin_modalbg.addEventListener("click", () => {
+  signin_modal.classList.remove("is-active");
+});
